b63952d1267578b2c234094d92753ae98c80289f
Scott Weinstein <sweinstein27@gmail.com>
2018-04-30 16:02:09 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,10 +3,8 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < collection.length
-    yield collection[i]
-    binding.pry
     i += 1
-    new_collection << collection[i]
+    new_collection << yield(collection[i])
   end
   new_collection
 end
