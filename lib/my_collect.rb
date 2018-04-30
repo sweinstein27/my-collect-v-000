1636e924d5c38866948d24f441407ed5b5ceb286
Scott Weinstein <sweinstein27@gmail.com>
2018-04-30 15:33:13 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,20 @@
-def my_collect(collection)
-  i = 0
-  new_collection = []
-  while i < collection.length
-    yield collection[i]
-    i += 1
-    collection[i] << new_collection
+# def my_collect(collection)
+#   i = 0
+#   new_collection = []
+#   while i < collection.length
+#     yield collection[i]
+#     i += 1
+#     collection[i] << new_collection
+#   end
+#   new_collection
+# end
+
+
+def my_collect
+    new_ary = []
+    self.each do |elem|
+      new_ary << yield(elem)
+    end
+    new_ary
   end
-  new_collection
 end
